99dc376839a49beef9cffabb6561c1a052c67ddc
edhemphill <edhemphill@wigwag.com>
2015-04-02 15:01:14 -0500

    with last commit

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-require('coffee-script');
-require('coffee-script/register');
-var i2c = require('./lib/i2c');
+// require('coffee-script');
+// require('coffee-script/register');
+var i2c = require('./lib/i2c.js');
 
 module.exports = i2c;
